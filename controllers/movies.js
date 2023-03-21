5d95a68427b3e778b4b98f92aea3d1473002fbbe
viveloper <ucsik1234@gmail.com>
2023-03-21 16:44:12 +0900

    fix: change movie detail api

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,11 @@
 const Movie = require('../models/movieModel.js');
 
+const ARTE_MOVIE_CODE_LIST = [
+  16079, 16076, 15964, 16232, 16176, 15600, 16056, 16106, 12869, 16233, 16132,
+  15558, 16097,
+];
+const OPERA_MOVIE_CODE_LIST = [12486];
+
 // @desc    Get movies
 // @route   GET /api/movies
 // @access  Public
@@ -10,44 +16,44 @@ exports.getMovies = async (req, res, next) => {
     ? undefined
     : Number(req.query.limit);
 
+  let findFilter = playing ? { MoviePlayYN: playing } : {};
+
   if (type === 'arte') {
-    const arteMovieCodeList = [
-      16079, 16076, 15964, 16232, 16176, 15600, 16056, 16106, 12869, 16233,
-      16132, 15558, 16097,
-    ];
-    const movies = await Movie.find({
-      $or: arteMovieCodeList.map((movieCode) => ({
+    findFilter = {
+      ...findFilter,
+      $or: ARTE_MOVIE_CODE_LIST.map((movieCode) => ({
         RepresentationMovieCode: movieCode,
       })),
-    })
-      .select('-Review -Casting -Trailer')
-      .sort({ ViewRate: -1 })
-      .limit(limit)
-      .exec();
-    return res.status(200).json(movies);
+    };
   } else if (type === 'opera') {
-    const movies = await Movie.find({
-      $or: [
-        {
-          RepresentationMovieCode: 12486,
-        },
-      ],
-    })
-      .select('-Review -Casting -Trailer')
-      .sort({ ViewRate: -1 })
-      .limit(limit)
-      .exec();
-    return res.status(200).json(movies);
+    findFilter = {
+      ...findFilter,
+      $or: OPERA_MOVIE_CODE_LIST.map((movieCode) => ({
+        RepresentationMovieCode: movieCode,
+      })),
+    };
   } else {
-    const movies = await Movie.find(
-      playing ? { MoviePlayYN: playing } : undefined
-    )
-      .select('-Review -Casting -Trailer')
-      .sort({ ViewRate: -1 })
-      .limit(limit)
-      .exec();
-    return res.status(200).json(movies);
   }
+
+  const movies = await Movie.find(findFilter)
+    .select(
+      'RepresentationMovieCode MovieNameKR PosterURL BookingRate ViewEvaluation DDay MoviePlayYN'
+    )
+    .sort({ ViewRate: -1 })
+    .limit(limit)
+    .exec();
+
+  return res.status(200).json(
+    movies.map((item) => ({
+      movieCode: item.RepresentationMovieCode,
+      movieName: item.MovieNameKR,
+      posterUrl: item.PosterURL,
+      bookingRate: item.BookingRate,
+      viewEvaluation: item.ViewEvaluation,
+      dday: Number(item.DDay),
+      moviePlayYN: item.MoviePlayYN,
+    }))
+  );
 };
 
 // @desc    Get movie detail
@@ -57,10 +63,41 @@ exports.getMovieDetail = async (req, res, next) => {
   const movieCode = req.params.movieCode;
 
   if (movieCode) {
-    const movie = await Movie.find({ RepresentationMovieCode: movieCode })
+    const movies = await Movie.find({ RepresentationMovieCode: movieCode })
       .select('-Review')
       .exec();
-    return res.status(200).json(movie);
+    if (!movies.length) {
+      return res.status(404).json({
+        message: 'Not Found.',
+      });
+    }
+    const movie = movies[0];
+    return res.status(200).json({
+      movieCode: movie.RepresentationMovieCode,
+      movieName: movie.MovieNameKR,
+      posterUrl: movie.PosterURL,
+      viewEvaluation: movie.ViewEvaluation,
+      bookingRate: movie.BookingRate,
+      dday: Number(movie.DDay),
+      moviePlayYN: movie.MoviePlayYN,
+      viewGradeCode: movie.ViewGradeCode,
+      cumulativeAudience: movie.KOFCustCnt,
+      likeCount: movie.LikeCount,
+      playTime: movie.PlayTime,
+      genreName: movie.MovieGenreNameKR,
+      genreName2: movie.MovieGenreNameKR2,
+      makingNationName: movie.MakingNationNameKR,
+      releaseDate: movie.ReleaseDate,
+      synopsis: movie.SynopsisKR,
+      manPrefer: Number(movie.ManPrefer),
+      womanPrefer: Number(movie.WomanPrefer),
+      agePrefer10: Number(movie.AgePrefer10),
+      agePrefer20: Number(movie.AgePrefer20),
+      agePrefer30: Number(movie.AgePrefer30),
+      agePrefer40: Number(movie.AgePrefer40),
+      casting: movie.Casting,
+      trailer: movie.Trailer,
+    });
   } else {
     res.status(400).json({
       message: 'required movieCode.',
